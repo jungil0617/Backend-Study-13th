4f46aed573a5d49a1e8630fac4a0e026d6ff6eb8
jungil0617 <jungil0617@naver.com>
2025-02-26 00:38:47 +0900

    Feat: InputHandler 구조 및 검증 추가

--- StringCalculator/src/input/InputHandler.java
+++ StringCalculator/src/input/InputHandler.java
@@ -1,23 +1,15 @@
 package input;
 
 import java.util.Scanner;
+import validation.Validator;
 
 public class InputHandler {
-    Scanner sc = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public String getInput() {
-        System.out.print("계산할 식을 입력해주세요 : ");
-
-        String input = sc.nextLine().trim();
-
-        if (input.isEmpty()) {
-            throw new IllegalArgumentException("입력값이 비어있습니다.");
-        }
+        String input = scanner.nextLine();
+        Validator.validatorInput(input);
 
         return input;
-    }
-
-    public void close() {
-        sc.close();
     }
 }
